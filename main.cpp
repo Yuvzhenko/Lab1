cb9c95eeab0777882b00cafae33a9397cfff0e39
Yuvzhenko <nazariyuvhenko@gmail.com>
2025-11-19 20:55:38 +0200

    Code documentation
    I've added Doxygen comments to core classes and methods.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include "graph.h"
 
+/**
+ * @brief Entry point of the graph application.
+ *
+ * The program tries to load a graph from file. If the file does not exist,
+ * it offers the user to create a new graph (list or matrix).
+ * Afterwards, the user can repeatedly:
+ *  - print the graph,
+ *  - add or remove edges,
+ *  - check connectivity,
+ *  - compute distance between two vertices,
+ *  - or recreate the graph.
+ *
+ * @return Exit status code (0 for normal termination, 1 when exiting via menu).
+ * @throws None.
+ * @example
+ * // Typical execution flow:
+ * // 1. User starts program.
+ * // 2. Chooses "Create new Graph as an Adjacency List".
+ * // 3. Adds edges and checks connectivity.
+ */
+
 int main() {
 	Graph grph;
 	char choice;
